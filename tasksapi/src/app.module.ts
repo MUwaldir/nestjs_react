38434fcd82c463b7ab2e7415113d3da232888aaa
waldirmerjildo <muwaldir@gmail.com>
2023-12-10 20:35:28 -0500

    subiendo el projecto backend de gestion de taraes con nestjs

--- tasksapi/src/app.module.ts
+++ tasksapi/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+config();
 
+const MONGODB_CONNECT_URI =
+  process.env.MONGODB_CONNECT_URI || 'mongodb://127.0.0.1:27017/taskdb';
 @Module({
   // imports: [MongooseModule.forRoot('mongodb://localhost/taskdb'), TasksModule],
-  imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/taskdb'),
-    TasksModule,
-  ],
+  imports: [MongooseModule.forRoot(MONGODB_CONNECT_URI), TasksModule],
 
   controllers: [],
   providers: [],
--- tasksapi/src/main.ts
+++ tasksapi/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
   console.log('start listening in port http://localhost:4000');
 }
 bootstrap();
